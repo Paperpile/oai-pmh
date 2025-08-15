f6fe1222a89d529d6e5a14678e0db32e0fcbd4bc
Gregory Jordan <gjuggler@gmail.com>
2025-08-15 00:47:08 -0400

    Small docs change

--- src/oai-pmh-xml.js
+++ src/oai-pmh-xml.js
@@ -12,17 +12,16 @@ const options = {
   ignoreNameSpace: false,
   allowBooleanAttributes: false,
   // parseFooValue controls whether fxp parses strings into numbers.
-  // We generally prefer to control that ourselves. 
+  // Callers generally prefer to be able to handle that as needed. 
   parseNodeValue: false,
   parseTagValue: false,
   parseAttributeValue: false,
   trimValues: true,
   stopNodes: ['article-title', 'abstract', 'body'],
-  // Let fast-xml-parser handle decoding HTML character entities.
-  htmlEntities: true,
+  // fast-xml-parser handles decoding XML entities.
+  processEntities: true,
 };
 const parser = new XMLParser(options);
-
 
 async function parseUsingFastParser(xml) {
   const parsedItem = parser.parse(xml);
